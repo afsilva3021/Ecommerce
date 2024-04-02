60105918531e1d459046d98651e9ee18a5dc5982
Alex felix <afsilva3021@gmail.com>
2024-04-02 17:35:05 -0300

    autização da paginas index

--- Views/Components/navbar.php
+++ Views/Components/navbar.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-lg bg-dark fs-5" data-bs-theme="dark">
     <div class="container-fluid ">
-        <a class="navbar-brand" href="#"><img src="https://img.freepik.com/vetores-premium/um-logotipo-para-uma-empresa-de-tecnologia-de-comercio-eletronico_557539-1697.jpg" id="logo" alt="logo" title="Ecommerce"></a>
+        <!-- <a class="navbar-brand" href="#"><img src="https://img.freepik.com/vetores-premium/um-logotipo-para-uma-empresa-de-tecnologia-de-comercio-eletronico_557539-1697.jpg" id="logo" alt="logo" title="Ecommerce"></a> -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -15,7 +15,7 @@
                     <a class="nav-link" href="#Feature">Promoções</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#Pricing">Destaqe</a>
+                    <a class="nav-link" href="#Pricing">Destaque</a>
                 </li>
                 <li class="nav-item dropdown opacity-75">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
